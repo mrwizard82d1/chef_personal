2dc631e0eff1275d82dc54418a6cd8d0da21dda5
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2015-06-06 23:35:54 -0500

    Add Double Commander disc image file.
    Add a `remote_file` resource that downloads the Mac OS X disc image file to my expected location (`~/professional/software/shells`).
    
    Woo hoo! It works.

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+
+
+case
+when node["platform"] == "windows"
+when node["platform"] == "mac_os_x"
+  remote_file "/Users/#{node['current_user']}/professional/software/shells/doublecmd-0.6.2-6025.qt.x86_64.dmg" do
+    source "http://sourceforge.net/projects/doublecmd/files/DC%20for%20Mac%20OS%20X%2064%20bit/Double%20Commander%200.6.2%20beta/doublecmd-0.6.2-6025.qt.x86_64.dmg/download"
+  end
+when node["platform"] == "x86_64-linux"
+end
